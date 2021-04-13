64831d3ddfb0ec80ead5f3a1774018429ecb1ab1
ƐqɿmƐ <70713233+3prm3@users.noreply.github.com>
2021-04-13 09:19:43 -0700

    Update motd.lua

--- src/main/resources/assets/computercraft/lua/rom/programs/motd.lua
+++ src/main/resources/assets/computercraft/lua/rom/programs/motd.lua
@@ -5,6 +5,22 @@ elseif date.month == 12 and date.day == 24 then
     print("Merry X-mas!")
 elseif date.month == 10 and date.day == 31 then
     print("OOoooOOOoooo! Spooky!")
+elseif date.month == 4 and date.day == 1 then
+    print("April Fools!")
+elseif date.month == 11 and date.day == 22 then
+    print("Happy Thanksgiving?")
+elseif date.month == 11 and date.day == 23 then
+    print("Happy Thanksgiving?"
+elseif date.month == 11 and date.day == 24 then
+    print("Happy Thanksgiving?"))
+elseif date.month == 11 and date.day == 25 then
+    print("Happy Thanksgiving?")
+elseif date.month == 11 and date.day == 26 then
+    print("Happy Thanksgiving?")
+elseif date.month == 11 and date.day == 27 then
+    print("Happy Thanksgiving?")
+elseif date.month == 11 and date.day == 28 then
+    print("Happy Thanksgiving?")
 else
     local tMotd = {}
 
